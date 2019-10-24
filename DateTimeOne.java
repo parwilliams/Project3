46367041d497f6546a8f260d1d782581e405af49
parwilliams <parker.williams@ou.edu>
2019-10-24 16:26:07 -0500

    Working dateTimeOfOtherCity()

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -1,18 +1,32 @@
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.SimpleTimeZone;
+import java.util.TimeZone;
 
 public class DateTimeOne extends MesoDateTimeOneAbstract {
 
+	private static final int CDT_CONVERSION = -5 * 60 * 60 * 1000;
+	private static final int BST_CONVERSION = 6 * 60 * 60 * 1000;
 	private Calendar calendar;
-	private SimpleTimeZone cdt;
 	private int milliseconds;
 	private static final int MILLI_CONVERSION = 1000;
 	private int seconds;
-
+	private SimpleDateFormat dateFormat;
+	private SimpleDateFormat hourFormat;
+	private SimpleTimeZone gmt;
+	private SimpleTimeZone bst;
+	private SimpleTimeZone cdt;
+	private HashMap<String, String> times = new HashMap<String, String>();
+	
 	public DateTimeOne() {
 		calendar = new GregorianCalendar();
-
+		dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm a");
+		hourFormat = new SimpleDateFormat("HH:mm");
+		gmt = new SimpleTimeZone(0, "GMT");
+		bst = new SimpleTimeZone(BST_CONVERSION, "BST");
+		cdt = new SimpleTimeZone(CDT_CONVERSION, "CDT");
 	}
 
 	public int getValueOfSecond() {
@@ -31,18 +45,71 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 
 	public void dateTimeNow() {
 		// TODO: Print Present Date and Time
-
+		System.out.println("Current Date/Time: " + dateFormat.format(calendar.getTime()));
 	}
 
-	public void dateTimeOfOtherCity() {
+	public void dateTimeOfOtherCity() { //Need Help
 		// TODO:Implement Method
+		
+		GregorianCalendar gmtCalendar = new GregorianCalendar(gmt);
+		GregorianCalendar bstCalendar = new GregorianCalendar(bst);
+		GregorianCalendar cdtCalendar = new GregorianCalendar(cdt);
+		
+		//gmtCalendar.computeTime();
+		
+		/*
+		System.out.println(gmtCalendar.get(Calendar.HOUR_OF_DAY));
+		System.out.println(bstCalendar.get(Calendar.HOUR_OF_DAY));
+		System.out.println(cdtCalendar.get(Calendar.HOUR_OF_DAY));
+		*/
+		
+		System.out.println("Time on Server: " + hourFormat.format(calendar.getTime()));
+		timeZoneToString(gmtCalendar, gmt);
+		timeZoneToString(bstCalendar, bst);
+		timeZoneToString(cdtCalendar, cdt);
+		
+		/*System.out.println(timeZoneToString("Time on Server", hourFormat.format(calendar.getTime())));
+		System.out.println(timeZoneToString("GMT", hourFormat.format(gmtCalendar.getTime())));
+		System.out.println(timeZoneToString("BST", hourFormat.format(bstCalendar.getTime())));
+		System.out.println(timeZoneToString("CDT", hourFormat.format(cdtCalendar.getTime())));
+		*/
+		
+		//System.out.println("Time on Server: " + timeFormat.format(calendar.getTime()));
+		//System.out.println("GMT: " + timeFormat.format(gmtCalendar.getTime()));
+		//System.out.println("BST (90E): " + timeFormat.format(bstCalendar.getTime()));
+		//System.out.println("CDT (90W): " + timeFormat.format(cdtCalendar.getTime()));
 	}
 
+	
+	
 	public void dateTimeDifferentZone() {
 		// TODO:Implement Method
+		//times.put("GMT", )
+		
 	}
 
 	public void timeZoneHashMap() {
 		// TODO:Implement Method
+	}
+	
+	
+	private void timeZoneToString(Calendar calendar, TimeZone timezone) {
+		System.out.print(timezone.getID() + ": ");
+		System.out.println(calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE));
+	}
+	
+	
+	
+	/*private void timeZoneToString(String timeZoneName, String formatedTime) {
+		String str = timeZoneName;
+		str = str.concat(": " + formatedTime);
+		return str;
+		}
+	*/
+		
+	
+	
+	private String timeZone(){
+		return "";
 	}
 }
