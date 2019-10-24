2923f4f297af3c578918b260db9e2b3f15ce869c
parwilliams <parker.williams@ou.edu>
2019-10-24 17:33:05 -0500

    Added methods to find times and completed dateTimeDifferentZone()

--- DateTimeOne.java
+++ DateTimeOne.java
@@ -7,7 +7,7 @@ import java.util.TimeZone;
 
 public class DateTimeOne extends MesoDateTimeOneAbstract {
 
-	private static final int CDT_CONVERSION = -5 * 60 * 60 * 1000;
+	private static final int CST_CONVERSION = -5 * 60 * 60 * 1000;
 	private static final int BST_CONVERSION = 6 * 60 * 60 * 1000;
 	private Calendar calendar;
 	private int milliseconds;
@@ -15,18 +15,28 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 	private int seconds;
 	private SimpleDateFormat dateFormat;
 	private SimpleDateFormat hourFormat;
+	private SimpleDateFormat minuteFormat;
+	private SimpleDateFormat hourMinuteFormat;
 	private SimpleTimeZone gmt;
 	private SimpleTimeZone bst;
-	private SimpleTimeZone cdt;
+	private SimpleTimeZone cst;
 	private HashMap<String, String> times = new HashMap<String, String>();
+	private GregorianCalendar gmtCalendar;
+	private GregorianCalendar bstCalendar;
+	private GregorianCalendar cstCalendar;
 	
 	public DateTimeOne() {
 		calendar = new GregorianCalendar();
-		dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm a");
-		hourFormat = new SimpleDateFormat("HH:mm");
+		dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm a");
+		hourFormat = new SimpleDateFormat("HH");
+		minuteFormat = new SimpleDateFormat("mm");
+		hourMinuteFormat = new SimpleDateFormat("HH:mm");
 		gmt = new SimpleTimeZone(0, "GMT");
 		bst = new SimpleTimeZone(BST_CONVERSION, "BST");
-		cdt = new SimpleTimeZone(CDT_CONVERSION, "CDT");
+		cst = new SimpleTimeZone(CST_CONVERSION, "CST");
+		gmtCalendar = new GregorianCalendar(gmt);
+		bstCalendar = new GregorianCalendar(bst);
+		cstCalendar = new GregorianCalendar(cst);
 	}
 
 	public int getValueOfSecond() {
@@ -51,10 +61,6 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 	public void dateTimeOfOtherCity() { //Need Help
 		// TODO:Implement Method
 		
-		GregorianCalendar gmtCalendar = new GregorianCalendar(gmt);
-		GregorianCalendar bstCalendar = new GregorianCalendar(bst);
-		GregorianCalendar cdtCalendar = new GregorianCalendar(cdt);
-		
 		//gmtCalendar.computeTime();
 		
 		/*
@@ -66,7 +72,7 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 		System.out.println("Time on Server: " + hourFormat.format(calendar.getTime()));
 		otherCityToString(gmtCalendar, gmt, "");
 		otherCityToString(bstCalendar, bst, " (90E)");
-		otherCityToString(cdtCalendar, cdt, " (90W)");
+		otherCityToString(cstCalendar, cst, " (90W)");
 		
 		/*System.out.println(timeZoneToString("Time on Server", hourFormat.format(calendar.getTime())));
 		System.out.println(timeZoneToString("GMT", hourFormat.format(gmtCalendar.getTime())));
@@ -79,24 +85,35 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 		//System.out.println("BST (90E): " + timeFormat.format(bstCalendar.getTime()));
 		//System.out.println("CDT (90W): " + timeFormat.format(cdtCalendar.getTime()));
 	}
-
 	
+	private void otherCityToString(Calendar calendar, TimeZone timezone, String degrees) {
+		System.out.print(timezone.getID() + degrees + ": ");
+		System.out.println(findHour(calendar, timezone) + ":" + findMinute(calendar, timezone));
+	}
+
 	
 	public void dateTimeDifferentZone() {
 		// TODO:Implement Method
-		//times.put("GMT", )
-		
+		times.put("GMT", dateTimeDifferentZoneToString(gmtCalendar, gmt));
+		times.put("BST", dateTimeDifferentZoneToString(bstCalendar, bst));
+		times.put("CST", dateTimeDifferentZoneToString(cstCalendar, cst));
+		System.out.println(times.get("GMT"));
+		System.out.println(times.get("BST"));
+		System.out.println(times.get("CST"));
 	}
 
+	private String dateTimeDifferentZoneToString(Calendar calendar, TimeZone timezone) {
+		String str = timezone.getID() + ": " + findCalendarMonth(calendar, timezone) + "/" + calendar.get(Calendar.DAY_OF_MONTH) + "/"
+				+ calendar.get(Calendar.YEAR) + " " + findHour(calendar, timezone) + ":" + findMinute(calendar, timezone);
+		return str;
+	}
+	
 	public void timeZoneHashMap() {
 		// TODO:Implement Method
 	}
 	
 	
-	private void otherCityToString(Calendar calendar, TimeZone timezone, String degrees) {
-		System.out.print(timezone.getID() + degrees + ": ");
-		System.out.println(calendar.get(Calendar.HOUR_OF_DAY) + ":" + calendar.get(Calendar.MINUTE));
-	}
+	
 	
 	
 	
@@ -107,9 +124,40 @@ public class DateTimeOne extends MesoDateTimeOneAbstract {
 		}
 	*/
 		
-	
-	
-	private String timeZone(){
-		return "";
+	private String findCalendarMonth(Calendar calendar, TimeZone timezone) {
+		String str;
+		int month;
+		if(calendar.get(Calendar.MONTH) < 9) {
+			month = calendar.get(Calendar.MONTH) + 1;
+			str = "0" + Integer.toString(month);
+			return str;
+		}
+		else
+			month = calendar.get(Calendar.MONTH) + 1;
+			str = Integer.toString(month);
+			return str;
 	}
+	
+	private String findHour(Calendar calendar, TimeZone timezone) {
+		String str;
+		if(calendar.get(Calendar.HOUR_OF_DAY) < 10) {
+			str = "0" + calendar.get(Calendar.HOUR_OF_DAY);
+			return str;
+		}
+		else
+			str = Integer.toString(calendar.get(Calendar.HOUR_OF_DAY));
+			return str;
+	}
+	
+	private String findMinute(Calendar calendar, TimeZone timezone) {
+		String str;
+		if(calendar.get(Calendar.MINUTE) < 10) {
+			str = "0" + calendar.get(Calendar.MINUTE);
+			return str;
+		}
+		else
+			str = Integer.toString(calendar.get(Calendar.MINUTE));
+			return str;
+	}
+	
 }
