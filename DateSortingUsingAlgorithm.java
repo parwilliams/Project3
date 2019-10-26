c82f47f03d29b6c51dd5cc3528de10a1ced79b08
parwilliams <parker.williams@ou.edu>
2019-10-26 14:01:21 -0500

    Implemented dateHashMapSorting

--- DateSortingUsingAlgorithm.java
+++ DateSortingUsingAlgorithm.java
@@ -5,18 +5,26 @@ import java.util.Scanner;
 
 public class DateSortingUsingAlgorithm {
 	private HashMap<LocalDate, Integer> dates;
+	private LocalDate[] dateKeys;
 	
 	public DateSortingUsingAlgorithm() {
 		dates = new HashMap<>();
+		dateKeys = new LocalDate[20];
 		try {
 			FileInputStream fis = new FileInputStream("SortingDates.txt");
 			Scanner scnr = new Scanner(fis);
 			int mapIndex = 1;
+			int arrayIndex = 0;
 			while (scnr.hasNext()) {
 				String dateString = scnr.next();
+				if(dateString.endsWith("-")) {
+					dateString = dateString.concat(scnr.next());
+				}
 				LocalDate fileDate = LocalDate.parse(dateString);
+				dateKeys[arrayIndex] = fileDate;
 				dates.put(fileDate, mapIndex);
 				mapIndex++;
+				arrayIndex++;
 			}
 		}
 		catch(Exception e) {
@@ -25,12 +33,38 @@ public class DateSortingUsingAlgorithm {
 	}
 
 	public void dateHashMapSortedDescending() {
-		// TODO Auto-generated method stub
+		int mapLength = dates.size();
+		for(int i = 0; i < mapLength - 1; i++) {
+			for(int j = 0; j < mapLength - i - 1; j++) {
+				if(dateKeys[j].isBefore(dateKeys[j + 1])){
+					LocalDate hold = dateKeys[j];
+					dateKeys[j] = dateKeys[j + 1];
+					dateKeys[j + 1] = hold;
+				}
+			}
+		}
+		
+		for(LocalDate key: dateKeys) {
+			System.out.println(key);
+		}
+		
 	}
 
 	public void dateHashMapSorted() {
-		// TODO Auto-generated method stub
+		int mapLength = dates.size();
+		for(int i = 0; i < mapLength - 1; i++) {
+			for(int j = 0; j < mapLength - i - 1; j++) {
+				if(dateKeys[j].isAfter(dateKeys[j + 1])){
+					LocalDate hold = dateKeys[j];
+					dateKeys[j] = dateKeys[j + 1];
+					dateKeys[j + 1] = hold;
+				}
+			}
+		}
 		
+		for(LocalDate key: dateKeys) {
+			System.out.println(key);
+		}
 	}
 	
 	
