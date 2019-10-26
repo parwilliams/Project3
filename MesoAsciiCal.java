0994f03beb1962f527925c3a38c431698df0d44a
parwilliams <parker.williams@ou.edu>
2019-10-26 12:06:36 -0500

    Implemented calAverage()

--- MesoAsciiCal.java
+++ MesoAsciiCal.java
@@ -1,13 +1,55 @@
 
-public class MesoAsciiCal extends MesoAsciiAbstract 
-{
+public class MesoAsciiCal extends MesoAsciiAbstract {
+	private String stationID;
+	private int inputAverage = -1;
+	private int fixedAverage = 79; //Set for Station "NRMN"
+
 	public MesoAsciiCal(MesoStation station) {
-		//TODO:Implement Constructor
+		// TODO:Implement Constructor
+		stationID = station.getStID();
 	}
-	
+
 	public int calAverage() {
-		//TODO:Implement Method
-		return 0;
+		int[] asciiCharacters = new int[4];
+		int ceiling = -1;
+		int floor = 1000;
+		double sumOfCharacters = 0;
+		double trueAverage;
+		double finalAverage;
+		int roundedFinalAverage;
+		
+		for (int i = 0; i < 4; i++) { // Loop to put character values in array
+			asciiCharacters[i] = stationID.charAt(i);
+		}
+
+		for (int i = 0; i < 4; i++) { // Add All characters
+			sumOfCharacters += asciiCharacters[i];
+		}
+
+		trueAverage = sumOfCharacters / 4;
+
+		ceiling = (int) Math.ceil(trueAverage);
+		floor = (int) Math.floor(trueAverage);
+
+		if ((sumOfCharacters % 4) >= 2) {
+			inputAverage = ceiling;
+		} else
+			inputAverage = floor;
+		
+		//Calculate average of input and fixed averages
+		finalAverage = ((double) inputAverage + (double) fixedAverage) / 2;
+		
+		//Reset ceiling and floor values
+		ceiling = (int) Math.ceil(finalAverage);
+		floor = (int) Math.floor(finalAverage);
+		
+		//Test to round
+		if ((sumOfCharacters % 4) >= 2) {
+			roundedFinalAverage = ceiling;
+		} else
+			roundedFinalAverage = floor;
+
+		return roundedFinalAverage;
 	}
-	
+
 }
