9fe16f670e634d25f1058f5108eeb0ca76d09baa
parwilliams <parker.williams@ou.edu>
2019-10-26 13:00:14 -0500

    Implemented sortedMap() and defined constructor

--- MesoLexicographical.java
+++ MesoLexicographical.java
@@ -1,17 +1,23 @@
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MesoLexicographical extends MesoSortedAbstract
 {
-
+	TreeMap<String, Integer> sortedVal;
+	
 	public MesoLexicographical(HashMap<String, Integer> asciiVal) {
-		// TODO Auto-generated constructor stub
+		sortedVal = new TreeMap<>(asciiVal);
+		for(Map.Entry<String, Integer> entry: sortedVal.entrySet()) {
+			System.out.println(entry.getKey());
+		}
+		
 	}
 
 	@Override
 	public Map<String, Integer> sortedMap(HashMap<String, Integer> unsorted) {
 		// TODO Auto-generated method stub
-		return null;
+		return sortedVal;
 	}	
 	
 }
