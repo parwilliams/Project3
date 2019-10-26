b82dc11f694dad4224da6d51547563b16a61047e
parwilliams <parker.williams@ou.edu>
2019-10-26 13:22:12 -0500

    Implemented constructor

--- DateSortingUsingAlgorithm.java
+++ DateSortingUsingAlgorithm.java
@@ -1,8 +1,27 @@
+import java.io.FileInputStream;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class DateSortingUsingAlgorithm {
+	private HashMap<LocalDate, Integer> dates;
 	
 	public DateSortingUsingAlgorithm() {
-		
+		dates = new HashMap<>();
+		try {
+			FileInputStream fis = new FileInputStream("SortingDates.txt");
+			Scanner scnr = new Scanner(fis);
+			int mapIndex = 1;
+			while (scnr.hasNext()) {
+				String dateString = scnr.next();
+				LocalDate fileDate = LocalDate.parse(dateString);
+				dates.put(fileDate, mapIndex);
+				mapIndex++;
+			}
+		}
+		catch(Exception e) {
+			e.toString();
+		}
 	}
 
 	public void dateHashMapSortedDescending() {
