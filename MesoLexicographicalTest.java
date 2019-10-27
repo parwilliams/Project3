295b570914b9cc49fee89aabfacf2ceee5e5e4c4
parwilliams <parker.williams@ou.edu>
2019-10-26 20:17:03 -0500

    Updated testSortedMap() and testMesoLexicographical()

--- MesoLexicographicalTest.java
+++ MesoLexicographicalTest.java
@@ -1,16 +1,28 @@
 import static org.junit.Assert.*;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
 import org.junit.Test;
 
 public class MesoLexicographicalTest {
-
+	private MesoEquivalent mesoEqual = new MesoEquivalent("OKCE");
+	private HashMap<String, Integer> testVal = mesoEqual.calAsciiEqual();
+	
 	@Test
 	public void testMesoLexicographical() {
-		fail("Not yet implemented");
+		MesoLexicographical expected = new MesoLexicographical(testVal);	
+		MesoLexicographical actual = new MesoLexicographical(mesoEqual.calAsciiEqual());	
+		assertEquals(expected, actual);
 	}
 
 	@Test
 	public void testSortedMap() {
-		fail("Not yet implemented");
+		MesoLexicographical actualLex = new MesoLexicographical(mesoEqual.calAsciiEqual());
+		TreeMap<String, Integer> expected = new TreeMap<>(testVal);
+		Map<String, Integer> actual = actualLex.sortedMap(testVal);
+		
+		assertEquals(expected, actual);
 	}
 }
