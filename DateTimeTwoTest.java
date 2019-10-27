5f0bc2fdcdb4e9899997afbe861967674bf84b97
parwilliams <parker.williams@ou.edu>
2019-10-26 19:21:17 -0500

    Updated testDaysOfAnyMonth()

--- DateTimeTwoTest.java
+++ DateTimeTwoTest.java
@@ -1,57 +1,34 @@
 import static org.junit.Assert.*;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import org.junit.Test;
 
 public class DateTimeTwoTest {
-
-	@Test
-	public void testGetValueOfSecond() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDateTimeNow() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSleepForFiveSec() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDateTimeOfOtherCity() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDateTimeDifferentZone() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testTimeZoneHashMap() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testDateTimeTwo() {
-		fail("Not yet implemented");
-	}
-
+	private DateTimeTwo dtt = new DateTimeTwo();
+	
 	@Test
 	public void testDaysOfCurrentMonth() {
-		fail("Not yet implemented");
+		dtt.daysOfCurrentMonth();
+		LocalDateTime currentDate = LocalDateTime.now();
+		LocalDate tenthDay = LocalDate.of(currentDate.getYear(), currentDate.getMonthValue(), 10);
+		LocalDate eighteenthDay = LocalDate.of(currentDate.getYear(), currentDate.getMonthValue(), 18);
+		equals("The tenth day of this month is " + tenthDay.getDayOfWeek() + 
+				" and eighteenth is " + eighteenthDay.getDayOfWeek());
 	}
+	
 
 	@Test
 	public void testDaysOfAnyMonth() {
-		fail("Not yet implemented");
+		dtt.daysOfAnyMonth(10, 2019);
+		equals("For the year (2019) and month (10), the fifteenth day is THURSDAY and the last day is Friday");
 	}
 
 	@Test
 	public void testCompareYear() {
-		fail("Not yet implemented");
+		dtt.compareYear();
+		//Ask How to write test for explicitly read Files
 	}
 
 	@Test
