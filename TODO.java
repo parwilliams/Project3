fefe80f41c2c57f9cb33faac5778ecd9fe0ae595
parwilliams <parker.williams@ou.edu>
2019-10-26 19:44:41 -0500

    Completed TODO, with flaws

---
+++ TODO.java
@@ -0,0 +1,50 @@
+
+public class TODO {
+	/*
+	 * TODO LIST:
+	 * 
+	 * DateTimeOne) 
+	 * 
+	 * constructor: X		
+	 * getValueOfSecond(): X
+	 * sleepForFiveSec(): X
+	 * dateTimeNow(): X
+	 * dateTimeOfOtherCity(): X
+	 * dateTimeDifferentZone(): X
+	 * timeZoneHashMap(): X
+	 * 
+	 * 
+	 * DateTimeTwo) 
+	 * 
+	 * constructor: X
+	 * daysOfCurrentMonth(): X
+	 * daysOfAnyMonth(): X
+	 * compareYear(): X
+	 * dateHashMap(): X
+	 * dateHashMapSorted(): X
+	 * 
+	 * 
+	 * MesoAsciiCal)
+	 * 
+	 *constructor: X
+	 *calAverage(): X
+	 *
+	 *
+	 *MesoEquivalent)
+	 *
+	 *constructor(): X
+	 *calAsciiEqual(): X
+	 *
+	 *
+	 *MesoLexigraphical)
+	 *
+	 *constructor(): X
+	 *sortedMap(): X
+	 *
+	 *
+	 *DataSortingUsingAlgorithm)
+	 *constructor(): X
+	 *dateHashMapSorted(): X
+	 *dateHashMapSortedDescending(): X
+	 */
+}
